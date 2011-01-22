16ed744d8328d476c43559f1c2a19487dd59cf12
Dustin Willis Webber <dustin.webber@gmail.com>
2011-01-22 18:48:48 -0500

    working on the welcome page

---
+++ lib/snorbyorg/versions.rb
@@ -0,0 +1,25 @@
+module Snorbyorg
+  
+  VERSION='0.1.0'
+  
+  module Versions
+    
+    def self.snorby
+      '2.2.1'
+    end
+    
+    def self.insta_snorby
+      '0.6.0'
+    end
+    
+    def self.snorby_download
+      "https://github.com/Snorby/snorby/zipball/#{Snorbyorg::Versions.snorby}"
+    end
+    
+    def self.insta_snorby_download
+      "http://www.snorby.org/Insta-Snorby-#{Snorbyorg::Versions.insta_snorby}.iso"
+    end
+    
+  end
+  
+end
